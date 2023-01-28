796a9eccb5d5de732f97bef477e3fb54b1b94512
Chris Woodruff <cwoodruff@live.com>
2023-01-27 19:30:26 -0500

    updated keyless-entity-types project to use Chinook db

---
+++ EFCoreDemos/keyless-entity-types/Chinook/AlbumWithArtistName.cs
@@ -0,0 +1,10 @@
+﻿#nullable disable
+
+namespace keylessentitytypes.Chinook;
+
+public class AlbumWithArtistName : BaseEntity
+{
+    public string Title { get; set; }
+    public int ArtistId { get; set; }
+    public string Name { get; set; }
+}
---
+++ EFCoreDemos/keyless-entity-types/Chinook/Artist.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace keylessentitytypes.Chinook;
+
+public sealed class Artist : BaseEntity
+{
+    public Artist()
+    {
+        Albums = new HashSet<Album>();
+    }
+
+    public string Name { get; set; }
+
+    public ICollection<Album> Albums { get; set; }
+}
