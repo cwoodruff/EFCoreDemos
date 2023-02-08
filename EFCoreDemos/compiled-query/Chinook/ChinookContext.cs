11414d74c432533f99ab1af0d82881c8c9781032
Chris Woodruff <cwoodruff@live.com>
2023-02-07 22:58:05 -0500

    updated compiled-query project for better original tests and also Benchmark .NET tests

--- EFCoreDemos/compiled-query/Chinook/ChinookContext.cs
+++ EFCoreDemos/compiled-query/Chinook/ChinookContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace compiled_query.Chinook;
 
@@ -234,29 +235,45 @@ public partial class ChinookContext : DbContext
             db.Playlists.Where(a => a.Id == id).SelectMany(t => t.Tracks!));
 
     public ChinookContext(DbContextOptions<ChinookContext> options)
-        : base(options)
-    {
-    }
+        : base(options) => Interlocked.Increment(ref InstanceCount);
+    
+    public static long InstanceCount;
 
-    public virtual DbSet<Album> Albums { get; set; } = null!;
-    public virtual DbSet<Artist> Artists { get; set; } = null!;
-    public virtual DbSet<Customer> Customers { get; set; } = null!;
-    public virtual DbSet<Employee> Employees { get; set; } = null!;
-    public virtual DbSet<Genre> Genres { get; set; } = null!;
-    public virtual DbSet<Invoice> Invoices { get; set; } = null!;
-    public virtual DbSet<InvoiceLine> InvoiceLines { get; set; } = null!;
-    public virtual DbSet<MediaType> MediaTypes { get; set; } = null!;
-    public virtual DbSet<Playlist> Playlists { get; set; } = null!;
-    public virtual DbSet<Track> Tracks { get; set; } = null!;
+    public virtual DbSet<Album> Albums { get; set; }
+
+    public virtual DbSet<Artist> Artists { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Genre> Genres { get; set; }
+
+    public virtual DbSet<Invoice> Invoices { get; set; }
+
+    public virtual DbSet<InvoiceLine> InvoiceLines { get; set; }
+
+    public virtual DbSet<MediaType> MediaTypes { get; set; }
+
+    public virtual DbSet<Playlist> Playlists { get; set; }
+
+    public virtual DbSet<Track> Tracks { get; set; }
+
+    private static readonly ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+    {
+        builder
+            .AddFilter(DbLoggerCategory.Database.Command.Name, LogLevel.Information);
+    });
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-    }
+        => optionsBuilder.UseLoggerFactory(loggerFactory);
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Album>(entity =>
         {
+            entity.HasKey(e => e.Id).HasName("PK__Album__97B4BE370AD2A005");
+
             entity.ToTable("Album");
 
             entity.HasIndex(e => e.ArtistId, "IFK_Artist_Album");
@@ -265,24 +282,39 @@ public partial class ChinookContext : DbContext
 
             entity.Property(e => e.Title).HasMaxLength(160);
 
-            entity.HasOne(d => d.Artist)
-                .WithMany(p => p.Albums)
+            entity.HasOne(d => d.Artist).WithMany(p => p.Albums)
                 .HasForeignKey(d => d.ArtistId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK__Album__ArtistId__276EDEB3");
+            
+            entity.HasMany<Track>(e => e.Tracks)
+                .WithOne(e => e.Album)
+                .HasForeignKey(e => e.AlbumId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Track__AlbumId__2B3F6F97");
         });
 
         modelBuilder.Entity<Artist>(entity =>
         {
+            entity.HasKey(e => e.Id).HasName("PK__Artist__25706B5007020F21");
+
             entity.ToTable("Artist");
 
             entity.HasIndex(e => e.Id, "IPK_Artist");
 
             entity.Property(e => e.Name).HasMaxLength(120);
+
+            entity.HasMany<Album>(e => e.Albums)
+                .WithOne(e => e.Artist)
+                .HasForeignKey(e => e.ArtistId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Album__ArtistId__276EDEB3");
         });
 
         modelBuilder.Entity<Customer>(entity =>
         {
+            entity.HasKey(e => e.Id).HasName("PK__Customer__A4AE64D8164452B1");
+
             entity.ToTable("Customer");
 
             entity.HasIndex(e => e.SupportRepId, "IFK_Employee_Customer");
@@ -290,35 +322,32 @@ public partial class ChinookContext : DbContext
             entity.HasIndex(e => e.Id, "IPK_Customer");
 
             entity.Property(e => e.Address).HasMaxLength(70);
-
             entity.Property(e => e.City).HasMaxLength(40);
-
             entity.Property(e => e.Company).HasMaxLength(80);
-
             entity.Property(e => e.Country).HasMaxLength(40);
-
             entity.Property(e => e.Email).HasMaxLength(60);
-
             entity.Property(e => e.Fax).HasMaxLength(24);
-
             entity.Property(e => e.FirstName).HasMaxLength(40);
-
             entity.Property(e => e.LastName).HasMaxLength(20);
-
             entity.Property(e => e.Phone).HasMaxLength(24);
-
             entity.Property(e => e.PostalCode).HasMaxLength(10);
-
             entity.Property(e => e.State).HasMaxLength(40);
 
-            entity.HasOne(d => d.SupportRep)
-                .WithMany(p => p.Customers)
+            entity.HasOne(d => d.SupportRep).WithMany(p => p.Customers)
                 .HasForeignKey(d => d.SupportRepId)
                 .HasConstraintName("FK__Customer__Suppor__2C3393D0");
+            
+            entity.HasMany<Invoice>(e => e.Invoices)
+                .WithOne(e => e.Customer)
+                .HasForeignKey(e => e.CustomerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Invoice__Customer__2D27B809");
         });
 
         modelBuilder.Entity<Employee>(entity =>
         {
+            entity.HasKey(e => e.Id).HasName("PK__Employee__7AD04F111273C1CD");
+
             entity.ToTable("Employee");
 
             entity.HasIndex(e => e.ReportsTo, "IFK_Employee_ReportsTo");
@@ -326,48 +355,51 @@ public partial class ChinookContext : DbContext
             entity.HasIndex(e => e.Id, "IPK_Employee");
 
             entity.Property(e => e.Address).HasMaxLength(70);
-
             entity.Property(e => e.BirthDate).HasColumnType("datetime");
-
             entity.Property(e => e.City).HasMaxLength(40);
-
             entity.Property(e => e.Country).HasMaxLength(40);
-
             entity.Property(e => e.Email).HasMaxLength(60);
-
             entity.Property(e => e.Fax).HasMaxLength(24);
-
             entity.Property(e => e.FirstName).HasMaxLength(20);
-
             entity.Property(e => e.HireDate).HasColumnType("datetime");
-
             entity.Property(e => e.LastName).HasMaxLength(20);
-
             entity.Property(e => e.Phone).HasMaxLength(24);
-
             entity.Property(e => e.PostalCode).HasMaxLength(10);
-
             entity.Property(e => e.State).HasMaxLength(40);
-
             entity.Property(e => e.Title).HasMaxLength(30);
 
-            entity.HasOne(d => d.ReportsToNavigation)
-                .WithMany(p => p.InverseReportsToNavigation)
+            entity.HasOne(d => d.ReportsToNavigation).WithMany(p => p.InverseReportsToNavigation)
                 .HasForeignKey(d => d.ReportsTo)
                 .HasConstraintName("FK__Employee__Report__2B3F6F97");
+            
+            entity.HasMany<Customer>(e => e.Customers)
+                .WithOne(e => e.SupportRep)
+                .HasForeignKey(e => e.SupportRepId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Customer__Suppor__2C3393D0");
         });
 
         modelBuilder.Entity<Genre>(entity =>
         {
+            entity.HasKey(e => e.Id).HasName("PK__Genre__0385057E7F60ED59");
+
             entity.ToTable("Genre");
 
             entity.HasIndex(e => e.Id, "IPK_Genre");
 
             entity.Property(e => e.Name).HasMaxLength(120);
+            
+            entity.HasMany<Track>(e => e.Tracks)
+                .WithOne(e => e.Genre)
+                .HasForeignKey(e => e.GenreId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Track__GenreId__2E1BDC42");
         });
 
         modelBuilder.Entity<Invoice>(entity =>
         {
+            entity.HasKey(e => e.Id).HasName("PK__Invoice__D796AAB51A14E395");
+
             entity.ToTable("Invoice");
 
             entity.HasIndex(e => e.CustomerId, "IFK_Customer_Invoice");
@@ -375,28 +407,29 @@ public partial class ChinookContext : DbContext
             entity.HasIndex(e => e.Id, "IPK_Invoice");
 
             entity.Property(e => e.BillingAddress).HasMaxLength(70);
-
             entity.Property(e => e.BillingCity).HasMaxLength(40);
-
             entity.Property(e => e.BillingCountry).HasMaxLength(40);
-
             entity.Property(e => e.BillingPostalCode).HasMaxLength(10);
-
             entity.Property(e => e.BillingState).HasMaxLength(40);
-
             entity.Property(e => e.InvoiceDate).HasColumnType("datetime");
-
             entity.Property(e => e.Total).HasColumnType("numeric(10, 2)");
 
-            entity.HasOne(d => d.Customer)
-                .WithMany(p => p.Invoices)
+            entity.HasOne(d => d.Customer).WithMany(p => p.Invoices)
                 .HasForeignKey(d => d.CustomerId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK__Invoice__Custome__2D27B809");
+            
+            entity.HasMany<InvoiceLine>(e => e.InvoiceLines)
+                .WithOne(e => e.Invoice)
+                .HasForeignKey(e => e.InvoiceId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__InvoiceLi__Invoi__2F10007B");
         });
 
         modelBuilder.Entity<InvoiceLine>(entity =>
         {
+            entity.HasKey(e => e.Id).HasName("PK__InvoiceL__0D760AD91DE57479");
+
             entity.ToTable("InvoiceLine");
 
             entity.HasIndex(e => e.InvoiceId, "IFK_Invoice_InvoiceLine");
@@ -407,14 +440,12 @@ public partial class ChinookContext : DbContext
 
             entity.Property(e => e.UnitPrice).HasColumnType("numeric(10, 2)");
 
-            entity.HasOne(d => d.Invoice)
-                .WithMany(p => p.InvoiceLines)
+            entity.HasOne(d => d.Invoice).WithMany(p => p.InvoiceLines)
                 .HasForeignKey(d => d.InvoiceId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK__InvoiceLi__Invoi__2F10007B");
 
-            entity.HasOne(d => d.Track)
-                .WithMany(p => p.InvoiceLines)
+            entity.HasOne(d => d.Track).WithMany(p => p.InvoiceLines)
                 .HasForeignKey(d => d.TrackId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK__InvoiceLi__Track__2E1BDC42");
@@ -422,45 +453,56 @@ public partial class ChinookContext : DbContext
 
         modelBuilder.Entity<MediaType>(entity =>
         {
+            entity.HasKey(e => e.Id).HasName("PK__MediaTyp__0E6FCB7203317E3D");
+
             entity.ToTable("MediaType");
 
             entity.HasIndex(e => e.Id, "IPK_MediaType");
 
             entity.Property(e => e.Name).HasMaxLength(120);
+            
+            entity.HasMany<Track>(e => e.Tracks)
+                .WithOne(e => e.MediaType)
+                .HasForeignKey(e => e.MediaTypeId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Track__MediaType__2D27B809");
         });
 
         modelBuilder.Entity<Playlist>(entity =>
         {
+            entity.HasKey(e => e.Id).HasName("PK__Playlist__B30167A021B6055D");
+
             entity.ToTable("Playlist");
 
             entity.HasIndex(e => e.Id, "IPK_Playlist");
 
             entity.Property(e => e.Name).HasMaxLength(120);
 
-            entity.HasMany(d => d.Tracks)
-                .WithMany(p => p.Playlists)
+            entity.HasMany(d => d.Tracks).WithMany(p => p.Playlists)
                 .UsingEntity<Dictionary<string, object>>(
                     "PlaylistTrack",
-                    l => l.HasOne<Track>().WithMany().HasForeignKey("TrackId").OnDelete(DeleteBehavior.ClientSetNull)
+                    r => r.HasOne<Track>().WithMany()
+                        .HasForeignKey("TrackId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
                         .HasConstraintName("FK__PlaylistT__Track__300424B4"),
-                    r => r.HasOne<Playlist>().WithMany().HasForeignKey("PlaylistId")
-                        .OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK__PlaylistT__Playl__30F848ED"),
+                    l => l.HasOne<Playlist>().WithMany()
+                        .HasForeignKey("PlaylistId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK__PlaylistT__Playl__30F848ED"),
                     j =>
                     {
                         j.HasKey("PlaylistId", "TrackId").HasName("PK__Playlist__A4A6282E25869641");
-
                         j.ToTable("PlaylistTrack");
-
                         j.HasIndex(new[] { "PlaylistId" }, "IFK_Playlist_PlaylistTrack");
-
                         j.HasIndex(new[] { "TrackId" }, "IFK_Track_PlaylistTrack");
-
                         j.HasIndex(new[] { "PlaylistId" }, "IPK_PlaylistTrack");
                     });
         });
 
         modelBuilder.Entity<Track>(entity =>
         {
+            entity.HasKey(e => e.Id).HasName("PK__Track__7A74F8E00EA330E9");
+
             entity.ToTable("Track");
 
             entity.HasIndex(e => e.AlbumId, "IFK_Album_Track");
@@ -472,26 +514,30 @@ public partial class ChinookContext : DbContext
             entity.HasIndex(e => e.Id, "IPK_Track");
 
             entity.Property(e => e.Composer).HasMaxLength(220);
-
             entity.Property(e => e.Name).HasMaxLength(200);
-
             entity.Property(e => e.UnitPrice).HasColumnType("numeric(10, 2)");
 
-            entity.HasOne(d => d.Album)
-                .WithMany(p => p.Tracks)
+            entity.HasOne(d => d.Album).WithMany(p => p.Tracks)
                 .HasForeignKey(d => d.AlbumId)
                 .HasConstraintName("FK__Track__AlbumId__286302EC");
 
-            entity.HasOne(d => d.Genre)
-                .WithMany(p => p.Tracks)
+            entity.HasOne(d => d.Genre).WithMany(p => p.Tracks)
                 .HasForeignKey(d => d.GenreId)
                 .HasConstraintName("FK__Track__GenreId__2A4B4B5E");
 
-            entity.HasOne(d => d.MediaType)
-                .WithMany(p => p.Tracks)
+            entity.HasOne(d => d.MediaType).WithMany(p => p.Tracks)
                 .HasForeignKey(d => d.MediaTypeId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK__Track__MediaType__29572725");
+            
+            entity.HasMany<InvoiceLine>(e => e.InvoiceLines)
+                .WithOne(e => e.Track)
+                .HasForeignKey(e => e.TrackId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__InvoiceLi__Track__2E1BDC42");
+
+            entity.HasMany<Playlist>(e => e.Playlists)
+                .WithMany(p => p.Tracks);
         });
 
         OnModelCreatingPartial(modelBuilder);
--- EFCoreDemos/compiled-query/Program.cs
+++ EFCoreDemos/compiled-query/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 using compiled_query.Chinook;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,7 +18,7 @@ public class Program
         _context = new ChinookContext(dbContextOptions);
 
         // Warm up
-        var artist = _context.Artists.First();
+        _context.Artists.First();
 
         RunTest(
             albumIDs =>
@@ -33,13 +35,13 @@ public class Program
             albumIDs =>
             {
                 // Create explicit compiled query
-                var query = EF.CompileQuery((ChinookContext context, int id)
+                var explicitQuery = EF.CompileQuery((ChinookContext context, int id)
                     => _context.Albums.FirstOrDefault(a => a.Id == id));
 
                 foreach (var id in albumIDs)
                 {
                     // Invoke the compiled query
-                    var artist = query(_context, id);
+                    var artist = explicitQuery(_context, id);
                 }
             },
             name: "Compiled EF Core Query");
@@ -48,14 +50,16 @@ public class Program
             albumIDs =>
             {
                 // Create explicit compiled query
-                var query = EF.CompileAsyncQuery((ChinookContext context, int id)
+                var compiledExplicitQuery = EF.CompileAsyncQuery((ChinookContext context, int id)
                     => context.Albums.FirstOrDefault(a => a.Id == id));
-
+                
+                List<Task> l = new List<Task>();
                 foreach (var id in albumIDs)
                 {
-                    // Invoke the async compiled query
-                    var artist = query(_context, id).Result;
+                    // Invoke the compiled async query
+                    l.Add(compiledExplicitQuery(_context, id));
                 }
+                Task.WaitAny(l.ToArray());
             },
             name: "Async Compiled EF Core Query");
 
@@ -73,15 +77,19 @@ public class Program
         RunTest(
             albumIDs =>
             {
+                List<Task> l = new List<Task>();
                 foreach (var id in albumIDs)
                 {
-                    // Invoke the compiled async query from DBContext
-                    var artist = _context.GetAlbumAsync(id).Result;
+                    // Invoke the compiled async query from DBContext;
+                    l.Add(_context.GetAlbumAsync(id));
                 }
+                Task.WaitAny(l.ToArray());
             },
             name: "DBContext Async Compiled EF Core Query");
+        
+        var summary = BenchmarkRunner.Run<CmpldQryBenchmark>();
     }
-
+    
     private static void RunTest(Action<int[]> test, string name)
     {
         var albumIDs = GetAlbumIDs(500);
@@ -101,5 +109,95 @@ public class Program
         IQueryable<Album> albums = Queryable.Take(_context!.Albums, count);
 
         return albums.AsEnumerable().Select(i => i.Id).ToArray();
+    }
+}
+
+public class CmpldQryBenchmark
+{
+    private int[] _albumIDs;
+    private static ChinookContext? _context;
+    
+    public CmpldQryBenchmark()
+    {
+        var builder = new DbContextOptionsBuilder<ChinookContext>();
+        builder.UseSqlServer(
+            "Server=.;Database=Chinook;Trusted_Connection=True;TrustServerCertificate=True;Application Name=EFCoreDemos");
+
+        var dbContextOptions = builder.Options;
+        _context = new ChinookContext(dbContextOptions);
+
+        // Warm up
+        _context.Artists.First();
+
+        _albumIDs = GetAlbumIDs(500);
+    }
+
+    private static int[] GetAlbumIDs(int count)
+    {
+        IQueryable<Album> albums = Queryable.Take(_context!.Albums, count);
+
+        return albums.AsEnumerable().Select(i => i.Id).ToArray();
+    }
+
+    [Benchmark]
+    public void RunTimeEFCoreQuery()
+    {
+        foreach (var id in _albumIDs)
+        {
+            // Use a regular auto-compiled query
+            var artist = _context.Albums.FirstOrDefault(a => a.Id == id);
+        }
+    }
+    
+    [Benchmark]
+    public void CompiledEFCoreQuery()
+    {
+        // Create explicit compiled query
+        var explicitQuery = EF.CompileQuery((ChinookContext context, int id)
+            => _context.Albums.FirstOrDefault(a => a.Id == id));
+
+        foreach (var id in _albumIDs)
+        {
+            // Invoke the compiled query
+            var artist = explicitQuery(_context, id);
+        }
+    }
+    
+    [Benchmark]
+    public void AsyncCompiledEFCoreQuery()
+    {
+        // Create explicit compiled query
+        var compiledExplicitQuery = EF.CompileAsyncQuery((ChinookContext context, int id)
+            => context.Albums.FirstOrDefault(a => a.Id == id));
+                
+        List<Task> l = new List<Task>();
+        foreach (var id in _albumIDs)
+        {
+            // Invoke the compiled async query
+            l.Add(compiledExplicitQuery(_context, id));
+        }
+        Task.WaitAny(l.ToArray());
+    }
+    
+    [Benchmark]
+    public void DBContextCompiledEFCoreQuery()
+    {
+        foreach (var id in _albumIDs)
+        {
+            // Invoke the compiled query from DBContext
+            var artist = _context.GetAlbum(id);
+        }
+    }
+    
+    [Benchmark]
+    public void DBContextAsyncCompiledEFCoreQuery()
+    {
+        List<Task> l = new List<Task>();
+        foreach (var id in _albumIDs)
+        {
+            // Invoke the compiled async query from DBContext;
+            l.Add(_context.GetAlbumAsync(id));
+        }
+        Task.WaitAny(l.ToArray());
     }
 }
