51386d372c49974c057c99c8d5d697f4bd586d39
Chris Woodruff <cwoodruff@live.com>
2023-02-09 06:47:27 -0500

    update to keyless-entity-types project

--- EFCoreDemos/keyless-entity-types/Program.cs
+++ EFCoreDemos/keyless-entity-types/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using keylessentitytypes.Chinook;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 namespace keylessentitytypes;
 
