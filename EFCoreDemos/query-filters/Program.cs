404ab376a845ef153451249fbc3a838265499680
Chris Woodruff <cwoodruff@live.com>
2023-02-09 06:47:59 -0500

    update to query-filter project

--- EFCoreDemos/query-filters/Program.cs
+++ EFCoreDemos/query-filters/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Demos.Chinook;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 #pragma warning disable 169
 
